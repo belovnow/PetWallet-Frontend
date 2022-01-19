3139473af5aa1d62902ef0616efe48444a2bd452
Aleksey Belov <belovnow@gmail.com>
2022-01-19 22:19:29 +0300

    Setup ThemeProvider

--- src/components/app/ui.tsx
+++ src/components/app/ui.tsx
@@ -1,32 +1,34 @@
 import WalletsList from "../wallet-list";
 import {  
   Container,
+  createMuiTheme,
   CssBaseline,
+  ThemeProvider,
 } from "@material-ui/core";
 import { Provider } from "react-redux";
 import store from "../../store";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import MainPage from "../../pages/main";
 import InfoPage from "../../pages/info";
-
-import { useStyles } from "./styles";
 import Header from "../header/ui";
 
-const App = () => {
-  const classes = useStyles();
+const App = () => {  
+  const theme = createMuiTheme();
 
   return (
     <Provider store={store}>
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
         <CssBaseline>
          <Header />
-          <Container className={classes.content}>
+          <Container>
             <Switch>
               <Route exact path="/" component={MainPage} />
               <Route path="/info" component={InfoPage} />
             </Switch>
           </Container>
         </CssBaseline>
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   );
